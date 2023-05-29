0e145eb597f99c65730b74bce4787d701307e3d2
Fahim <fahimshahariar.fs@gmail.com>
2023-05-30 00:19:13 +0600

    eighth

--- f_mart_ecommerce/lib/constants/lists.dart
+++ f_mart_ecommerce/lib/constants/lists.dart
@@ -9,3 +9,9 @@ const socialIconList = [
 const sliderList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
 
 const scndsliderList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+const featuredimgList1 = [imgS1, imgS2, imgS3];
+const featuredimgList2 = [imgS4, imgS5, imgS6];
+
+const  featuredtitleList1 = [women, girlsDress, girls];
+const  featuredtitleList2 = [boys, mobile, tshirt];
--- f_mart_ecommerce/lib/screens/home screen/homescreen.dart
+++ f_mart_ecommerce/lib/screens/home screen/homescreen.dart
@@ -4,6 +4,7 @@ import 'package:f_mart_ecommerce/widgets/homeButtons.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/lists.dart';
+import 'components/featuredbutton.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,7 +25,7 @@ class HomeScreen extends StatelessWidget {
               color: lightGrey,
               child: TextFormField(
                 decoration: InputDecoration(
-                  border: InputBorder.none,
+                    border: InputBorder.none,
                     filled: true,
                     fillColor: Colors.white,
                     hintText: search,
@@ -55,8 +56,8 @@ class HomeScreen extends StatelessWidget {
                             .rounded
                             .clip(Clip.antiAlias)
                             .margin(
-                          EdgeInsets.symmetric(horizontal: 8),
-                        )
+                              EdgeInsets.symmetric(horizontal: 8),
+                            )
                             .make();
                       },
                     ),
@@ -64,15 +65,16 @@ class HomeScreen extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children:
-                      //error can be solved regarding list.generate by just removing the brackets of the children
-                      List.generate(
-                          2,
+                          //error can be solved regarding list.generate by just removing the brackets of the children
+                          List.generate(
+                              2,
                               (int index) => homebuttons(
-                            height: context.screenHeight * 0.15,
-                            width: context.screenWidth / 2.5,
-                            icon: index == 0 ? icTodaysDeal : icFlashDeal,
-                            title: index == 0 ? today : flash,
-                          )),
+                                    height: context.screenHeight * 0.15,
+                                    width: context.screenWidth / 2.5,
+                                    icon:
+                                        index == 0 ? icTodaysDeal : icFlashDeal,
+                                    title: index == 0 ? today : flash,
+                                  )),
                     ),
                     10.heightBox,
                     VxSwiper.builder(
@@ -90,8 +92,8 @@ class HomeScreen extends StatelessWidget {
                             .rounded
                             .clip(Clip.antiAlias)
                             .margin(
-                          EdgeInsets.symmetric(horizontal: 8),
-                        )
+                              EdgeInsets.symmetric(horizontal: 8),
+                            )
                             .make();
                       },
                     ),
@@ -100,22 +102,22 @@ class HomeScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: List.generate(
                         3,
-                            (index) => homebuttons(
+                        (index) => homebuttons(
                             height: context.screenHeight * 0.15,
                             width: context.screenWidth / 3.5,
                             icon: index == 0
                                 ? icTopCategories
                                 : index == 1
-                                ? icBrands
-                                : icTopSeller,
+                                    ? icBrands
+                                    : icTopSeller,
                             title: index == 0
                                 ? topC
                                 : index == 1
-                                ? brand
-                                : top),
+                                    ? brand
+                                    : top),
                       ),
                     ),
-                    10.heightBox,
+                    20.heightBox,
                     Align(
                       alignment: Alignment.centerLeft,
                       child: feature.text
@@ -123,7 +125,143 @@ class HomeScreen extends StatelessWidget {
                           .size(18)
                           .fontFamily(semibold)
                           .make(),
-                    )
+                    ),
+                    20.heightBox,
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                          children: List.generate(
+                        3,
+                        (index) => Column(
+                          children: [
+                            featuredButton(
+                                icon: featuredimgList1[index],
+                                title: featuredtitleList1[index]),
+                            10.heightBox,
+                            featuredButton(
+                                icon: featuredimgList2[index],
+                                title: featuredtitleList2[index]),
+                          ],
+                        ),
+                      ).toList()),
+                    ),
+                    20.heightBox,
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          featuredProduct.text
+                              .fontFamily(bold)
+                              .size(18)
+                              .white
+                              .make(),
+                          10.heightBox,
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: List.generate(
+                                6,
+                                (index) => Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset(
+                                      imgP1,
+                                      width: 150,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    10.heightBox,
+                                    "Laptop 8GB/512GB"
+                                        .text
+                                        .fontFamily(semibold)
+                                        .color(darkFontGrey)
+                                        .make(),
+                                    10.heightBox,
+                                    "50,000 tk"
+                                        .text
+                                        .fontFamily(bold)
+                                        .size(16)
+                                        .color(Colors.red)
+                                        .make()
+                                  ],
+                                )
+                                    .box
+                                    .margin(EdgeInsets.symmetric(horizontal: 4))
+                                    .white
+                                    .roundedSM
+                                    .padding(EdgeInsets.all(8))
+                                    .make(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    20.heightBox,
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 150,
+                      enlargeCenterPage: true,
+                      itemCount: scndsliderList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          scndsliderList[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(
+                              EdgeInsets.symmetric(horizontal: 8),
+                            )
+                            .make();
+                      },
+                    ),
+                    20.heightBox,
+                    GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 6,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 8,
+                          crossAxisSpacing: 8,
+                          mainAxisExtent: 300),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Image.asset(
+                              imgP5,
+                              width: 200,
+                              fit: BoxFit.fill,
+                            ),
+                            Spacer(),
+                            "Laptop 8GB/512GB"
+                                .text
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .make(),
+                            10.heightBox,
+                            "50,000 tk"
+                                .text
+                                .fontFamily(bold)
+                                .size(16)
+                                .color(Colors.red)
+                                .make()
+                          ],
+                        )
+                            .box
+                            .white
+                            .margin(EdgeInsets.symmetric(horizontal: 4)).padding(EdgeInsets.all(12))
+                            .roundedSM
+                            .make();
+                      },
+                    ),
                   ],
                 ),
               ),
